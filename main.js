000a13b85164b74ceed5e7afd81bc1ff505c6b8c
Biscolad <biscolad@gmail.com>
2023-11-04 04:24:09 -0400

    first html commit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function shuffleFrames() {
+    const itemContainer = document.querySelector('.item-Container');
+    for (let i = itemContainer.children.length; i>=0; i--) {
+        itemContainer.appendChild(itemContainer.children[Math.random() * i | 0]);
+    }
+}
+shuFFleFrames();
