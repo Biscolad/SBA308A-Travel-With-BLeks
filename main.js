a7676500935a92634fddedec8a6ae4d78d76a6e1
Biscolad <biscolad@gmail.com>
2023-11-05 01:09:09 -0500

    backgroundcolor and frames

--- main.js
+++
@@ -1,8 +0,0 @@
-
-function shuffleFrames() {
-    const itemContainer = document.querySelector('.item-Container');
-    for (let i = itemContainer.children.length; i>=0; i--) {
-        itemContainer.appendChild(itemContainer.children[Math.random() * i | 0]);
-    }
-}
-shuffleFrames();
