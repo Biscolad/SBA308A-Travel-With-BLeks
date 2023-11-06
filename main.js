4332b8d3f7f8dc07bc87711294a221c6c648a817
Biscolad <biscolad@gmail.com>
2023-11-06 15:18:31 -0500

    codes updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // code snippets from API website
-const url = 'https://travel-info-api.p.rapidapi.com/find-embassy?source=turkey&destination=usa';
+const url = 'https://travel-info-api.p.rapidapi.com/country-details';
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,39 +10,65 @@ const options = {
 
 
 // using fetch API to communicate with an external API
-async function fetchData() {
+async function fetchCountryData() {
     try {
-        const response = await fetch(url);
+        const response = await fetch('https://travel-info-api.p.rapidapi.com/country-details');
         if (!response.ok) {
-            throw new Error ('HTTP error!: error');
+            throw new Error('Error in fetching data');
         }
+       
         const result = await response.json();
+
         console.log(result);
         // const data = await response.json();
 
     } catch (error) {
-        console.error(error);
+        console.error('Error fetching country data:', error);
     }
   }
-  fetchData();
-  
-
+ 
 
 //CREATE USER INTERACTION
 // ADD Event listener for COUNTRY SEARCH
-const searchDestination = document.getElementById('destinationCountry');
-searchDestination.addEventListener('change', async() => {
-    const selectedCountry = searchDestination.value;
-    const response = await fetch(url);
-    if (response.ok) {
-        const result = await response.json();
-    }
+const destinationInput = document.getElementById('destinationCountry');
+const searchButton = document.getElementById('country-Search');
+searchButton.addEventListener('click', async() => {
+    const destination = destinationInput.value;
+    const countryData = await fetchCountryData(destination);
+    console.log(countryData);
 });
+
+
+//USER MANIPULATION
+
+
    
 
   
 
 //ENABLE USER MANIPULATION THROUGH POST REQUEST
+// In main.js
+// const form = document.getElementById('yourFormId');
+
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   const formData = new FormData(form);
+//   const postData = Object.fromEntries(formData);
+
+//   const response = await fetch('API_URL_FOR_POST_REQUEST', {
+//     method: 'POST',
+//     body: JSON.stringify(postData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+
+//   if (response.ok) {
+//     // Handle the response if needed
+//   }
+// });
+
 
 
 
